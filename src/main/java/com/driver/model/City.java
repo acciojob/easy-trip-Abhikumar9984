6cb1704b69bada90cdb50acb275ca88d80386598
Abhishek_ <vsukumar0991@gmail.com>
2024-01-04 13:49:05 +0530

    Code added

--- src/main/java/com/driver/model/City.java
+++ src/main/java/com/driver/model/City.java
@@ -2,81 +2,12 @@ package com.driver.model;
 
 public enum City {
     ;
+     String KANPUR;
+    String DELHI;
+    String  CHANDIGARH;
+   String  BANGLORE;
+   String JAIPUR;
+   String KOLKATA;
+   String  CHENNAI;
 
-    private String KANPUR;
-   private String DELHI;
-   private String  CHANDIGARH;
-   private String  BANGLORE;
-   private String JAIPUR;
-   private String KOLKATA;
-   private String  CHENNAI;
-
-    City() {
-    }
-
-    City(String KANPUR, String DELHI, String CHANDIGARH, String BANGLORE, String JAIPUR, String KOLKATA, String CHENNAI) {
-        this.KANPUR = KANPUR;
-        this.DELHI = DELHI;
-        this.CHANDIGARH = CHANDIGARH;
-        this.BANGLORE = BANGLORE;
-        this.JAIPUR = JAIPUR;
-        this.KOLKATA = KOLKATA;
-        this.CHENNAI = CHENNAI;
-    }
-
-    public String getKANPUR() {
-        return KANPUR;
-    }
-
-    public void setKANPUR(String KANPUR) {
-        this.KANPUR = KANPUR;
-    }
-
-    public String getDELHI() {
-        return DELHI;
-    }
-
-    public void setDELHI(String DELHI) {
-        this.DELHI = DELHI;
-    }
-
-    public String getCHANDIGARH() {
-        return CHANDIGARH;
-    }
-
-    public void setCHANDIGARH(String CHANDIGARH) {
-        this.CHANDIGARH = CHANDIGARH;
-    }
-
-    public String getBANGLORE() {
-        return BANGLORE;
-    }
-
-    public void setBANGLORE(String BANGLORE) {
-        this.BANGLORE = BANGLORE;
-    }
-
-    public String getJAIPUR() {
-        return JAIPUR;
-    }
-
-    public void setJAIPUR(String JAIPUR) {
-        this.JAIPUR = JAIPUR;
-    }
-
-    public String getKOLKATA() {
-        return KOLKATA;
-    }
-
-    public void setKOLKATA(String KOLKATA) {
-        this.KOLKATA = KOLKATA;
-    }
-
-    public String getCHENNAI() {
-        return CHENNAI;
-    }
-
-    public void setCHENNAI(String CHENNAI) {
-        this.CHENNAI = CHENNAI;
-    }
 }
