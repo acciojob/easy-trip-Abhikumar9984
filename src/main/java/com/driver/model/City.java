eb3bdc19ebf4471930b5fa1c639c85455cfed7fa
Abhishek_ <vsukumar0991@gmail.com>
2024-01-04 15:33:31 +0530

    Abhishek's Fourth Commit

--- src/main/java/com/driver/model/City.java
+++ src/main/java/com/driver/model/City.java
@@ -1,13 +1,12 @@
 package com.driver.model;
 
-public enum City {
-    ;
-    public static String KANPUR;
-    public static String DELHI;
-    public static String  CHANDIGARH;
-   public static String  BANGLORE;
-   public static String JAIPUR;
-   public static String KOLKATA;
-   public static String  CHENNAI;
 
+public enum City {
+    KANPUR,
+    DELHI,
+    CHANDIGARH,
+    BANGLORE,
+    JAIPUR,
+    KOLKATA,
+    CHENNAI,
 }
